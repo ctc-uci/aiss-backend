e2e0b73f988db4cb9cd213a38c38ea40aa0ca523
Cheryl Chen <cherylchen@Cheryls-MacBook-Pro-M2.local>
2023-11-18 22:57:09 -0800

    added drop table if exists

--- server/schema/users.sql
+++ server/schema/users.sql
@@ -1,6 +1,8 @@
 CREATE TYPE account_type as ENUM ('superadmin', 'admin');
 
-CREATE TABLE IF NOT EXISTS users (
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
 	id VARCHAR ( 256 ) PRIMARY KEY,
 	email VARCHAR ( 50 ) NOT NULL,
 	type account_type NOT NULL, 
