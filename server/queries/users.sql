d3113433328edbcd925a0ccd04cafcb9349f0c79
Philip Jian <psjian@uci.edu>
2023-11-22 23:06:27 -0800

    added Get, Get pending accounts, and Post

--- server/queries/users.sql
+++ server/queries/users.sql
@@ -11,3 +11,19 @@ DELETE FROM
     users
 WHERE
     id = ?;
+
+-- GET - Returns all data from the Users table
+SELECT * FROM
+    Users;
+
+-- GET/pending-accounts - Returns all data from Users table who are currently pending approval
+SELECT * FROM
+    Users
+WHERE
+    approved = false;
+
+-- POST - Adds a new row into Users table
+INSERT INTO
+    Users (id, email, "type", approved)
+VALUES
+    (?,?,?,?);
