57cc99696d1da01ff9337dd14ab2c31fb598d7fe
michellelin1 <michellelin810@gmail.com>
2023-11-26 23:21:47 -0800

    fixed published schedule types

--- server/schema/published_schedule.sql
+++ server/schema/published_schedule.sql
@@ -1,14 +1,11 @@
 DROP TABLE IF EXISTS published_schedule;
-
-CREATE TYPE year AS ENUM ('Junior','Senior');
-
 CREATE TABLE IF NOT EXISTS published_schedule (
     id varchar(10) NOT NULL,
     event_id varchar(10) NOT NULL,
     confirmed boolean NOT NULL,
-    confirmed_on DATETIME NOT NULL,
-    start_time DATETIME NOT NULL,
-    end_time DATETIME NOT NULL,
+    confirmed_on date NOT NULL,
+    start_time timestamp NOT NULL,
+    end_time timestamp NOT NULL,
     cohort year NOT NULL,
     notes varchar(100),
     FOREIGN KEY (event_id)
