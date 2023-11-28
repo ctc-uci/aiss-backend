726749f1dd193ae175501d38240231ec7d5042c7
Alyssia Tan <aly.tan@outlook.com>
2023-11-27 16:59:18 -0800

    created catalog.js, made progress

---
+++ routes/catalog.js
@@ -0,0 +1,70 @@
+const { toCamel } = require('../common/utils');
+const express = require('express');
+const catalogRouter = express.Router();
+
+
+// -- GET - Returns all data from the catalog table
+catalogRouter.get('/', async (req, res) => {
+  try {
+    const allInfo = await db.query(`SELECT * from catalog;`);
+    res.status(200).json(toCamel(allInfo));
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+});
+
+
+// -- GET/:id - Returns the row that matches the id
+// SELECT * FROM catalog WHERE id = ?;
+catalogRouter.get('/:id', async (req, res) => {
+    try {
+      const allUsers = await db.query(`SELECT * FROM catalog WHERE id = ?;`);
+      res.status(200).json(keysToCamel(allUsers));
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err.message);
+    }
+  });
+
+// -- POST - Adds a new row to the catalog table
+// INSERT INTO catalog (id, host, title, event_type, subject, description, year)
+// VALUES (?, ?, ?, ?, ?, ?, ?);
+catalogRouter.post('/', async (req, res) => {
+    try {
+      const { id, host, title, event_type, subject, description, year } = req.body;
+      
+    } catch (err) {
+      
+    }
+  });
+
+// -- PUT - Updates an existing row given an id
+// -- All fields are optional
+// UPDATE catalog
+// SET host = ?, title = ?, event_type = ?, subject = ?, description = ?, year = ?
+// WHERE id = ?;
+catalogRouter.put('/', async (req, res) => {
+    try {
+      const allUsers = await db.query(`SELECT * from catalog;`);
+      res.status(200).json(keysToCamel(allUsers));
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err.message);
+    }
+  });
+
+
+// -- DELETE - deletes an existing row given an id
+// DELETE FROM catalog WHERE id=?;
+catalogRouter.delete('/', async (req, res) => {
+    try {
+      const delUser = await db.query(`DELETE FROM catalog WHERE id = ${id};`);
+      res.status(200).json(keysToCamel(delUser));
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err.message);
+    }
+  });
+
+module.exports = catalogRouter;
