8ac1bd37afee6d29f8371d7ece2c752e283955b6
subinqkim <subinlk@uci.edu>
2023-12-28 22:51:39 -0800

    Updated package.json to include nodeMailer dependency, added endpoint for emailRouter in app.js, set cors credentials to true so it doesn't block req

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 // routes
 const users = require('./routes/users');
 
+const email = require('./routes/nodeMailer');
+
 const app = express();
 
 const catalogRouter = require('./routes/catalog');
@@ -15,14 +17,19 @@ const PORT = process.env.PORT || 3001;
 app.use(
   cors({
     origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
+    credentials: true,
   }),
 );
+
+// app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 // add all routes under here
 app.use(express.json()); // for req.body
 app.use('/users', users);
 
 app.use('/catalog', catalogRouter);
+
+app.use('/nodeMailer', email);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
--- routes/nodeMailer.js
+++ routes/nodeMailer.js
@@ -1,13 +1,23 @@
 const express = require('express');
+// const cors = require('cors');
 const transporter = require('../transporter');
 // TODO: add verifyToken
 
 const emailRouter = express();
+// emailRouter.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   }),
+// );
 
 emailRouter.use(express.json());
 
 emailRouter.post('/send', (req, res) => {
   const { email, messageHtml, subject } = req.body;
+  console.log('req.body', req.body);
+  console.log('email', email);
   const mail = {
     from: `${process.env.REACT_APP_EMAIL_FIRST_NAME} ${process.env.REACT_APP_EMAIL_LAST_NAME} ${process.env.REACT_APP_EMAIL_USERNAME}`,
     to: email,
