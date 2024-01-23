3ef8a11c837626eb3c6d2eb3d5acee0e523965a3
michellelin1 <66575725+michellelin1@users.noreply.github.com>
2024-01-23 13:56:07 -0800

    updated catalog schema + routes

--- server/schema/catalog.sql
+++ server/schema/catalog.sql
@@ -1,6 +1,7 @@
 CREATE TYPE event AS ENUM ('guest speaker', 'study-trip', 'workshop', 'other');
 CREATE TYPE subject AS ENUM ('life skills', 'science', 'technology', 'engineering', 'math', 'college readiness');
 CREATE TYPE year AS ENUM ('junior', 'senior', 'both');
+CREATE TYPE season AS ENUM ('spring', 'summer', 'fall', 'winter');
 
 DROP TABLE IF EXISTS catalog;
 CREATE TABLE catalog (
@@ -10,5 +11,7 @@ CREATE TABLE catalog (
   event_type event NOT NULL,
   subject subject NOT NULL,
   description VARCHAR(50) NOT NULL,
-  year year NOT NULL
+  year year NOT NULL,
+  season season,
+  location VARCHAR(256)
 );
